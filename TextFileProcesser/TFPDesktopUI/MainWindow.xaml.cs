9f9cf4a5fb3d5de925a462d4f27586785a41483b
Balázs Begov <bbegov@googlemail.com>
2022-09-27 11:42:34 +0200

    Layout created with two buttons

--- TextFileProcesser/TFPDesktopUI/MainWindow.xaml.cs
+++ TextFileProcesser/TFPDesktopUI/MainWindow.xaml.cs
@@ -1,15 +1,53 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Windows;
 using TFPLibrary;
 
 namespace TFPDesktopUI;
 
 public partial class MainWindow : Window
 {
-    private readonly IDataAccess _dataAccess;
+    private readonly IFileHandler _fileHandler;
 
-    public MainWindow(IDataAccess dataAccess)
+    public MainWindow(IFileHandler fileHandler)
     {
         InitializeComponent();
-        _dataAccess = dataAccess;
+        _fileHandler = fileHandler;
+    }
+
+    private void BrowseFile_Click(object sender, RoutedEventArgs e)
+    {
+        var dialog = new Microsoft.Win32.OpenFileDialog
+        {
+            FileName = "Document",
+            DefaultExt = ".txt",
+            Filter = "Text documents (.txt)|*.txt"
+        };
+
+        var result = dialog.ShowDialog();
+
+        if (result != true) return;
+
+        FilePath.Text = dialog.FileName;
+        ResultWindow.Text = string.Empty;
+    }
+
+    private void Analyze_Click(object sender, RoutedEventArgs e)
+    {
+        if (FilePath.Text == string.Empty)
+        {
+            ResultWindow.Text = "No file was chosen";
+            return;
+        }
+
+        var content = _fileHandler.ReadFile(FilePath.Text);
+        
+        if (content.Length == 0)
+        {
+            ResultWindow.Text = "File is empty";
+            return;
+        }
+        
+        ResultWindow.Text = content;
     }
 }
