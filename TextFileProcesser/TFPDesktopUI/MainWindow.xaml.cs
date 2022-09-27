a00c784a679a8e79513e96bf18005746de583a54
Balázs Begov <bbegov@googlemail.com>
2022-09-27 21:12:49 +0200

    Add: code added for button clicks (Browse, Analyse)

--- TextFileProcesser/TFPDesktopUI/MainWindow.xaml.cs
+++ TextFileProcesser/TFPDesktopUI/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.IO;
-using System.Windows;
+﻿using System.Windows;
+using TFPDesktopUI.Models;
 using TFPLibrary;
 
 namespace TFPDesktopUI;
@@ -8,15 +7,20 @@ namespace TFPDesktopUI;
 public partial class MainWindow : Window
 {
     private readonly IFileHandler _fileHandler;
+    private readonly ITextProcessor _textProcessor;
 
-    public MainWindow(IFileHandler fileHandler)
+    public MainWindow(IFileHandler fileHandler, ITextProcessor textProcessor)
     {
         InitializeComponent();
         _fileHandler = fileHandler;
+        _textProcessor = textProcessor;
+        CollapseResults();
     }
 
     private void BrowseFile_Click(object sender, RoutedEventArgs e)
     {
+        CollapseResults();
+
         var dialog = new Microsoft.Win32.OpenFileDialog
         {
             FileName = "Document",
@@ -24,30 +28,57 @@ public partial class MainWindow : Window
             Filter = "Text documents (.txt)|*.txt"
         };
 
-        var result = dialog.ShowDialog();
+        var isFileChosen = dialog.ShowDialog();
 
-        if (result != true) return;
+        if (isFileChosen == false) return;
 
         FilePath.Text = dialog.FileName;
-        ResultWindow.Text = string.Empty;
     }
 
     private void Analyze_Click(object sender, RoutedEventArgs e)
     {
+        CollapseResults();
+
         if (FilePath.Text == string.Empty)
         {
-            ResultWindow.Text = "No file was chosen";
+            ResultMessage.Text = "Browse a file first!";
+            ResultMessage.Visibility = Visibility.Visible;
             return;
         }
 
-        var content = _fileHandler.ReadFile(FilePath.Text);
+        var content = _fileHandler.ReadFileByLines(FilePath.Text);
         
         if (content.Length == 0)
         {
-            ResultWindow.Text = "File is empty";
+            ResultMessage.Text = "File is empty.";
+            ResultMessage.Visibility = Visibility.Visible;
             return;
         }
+
+        const string delimiter = " ";
+        var words = _textProcessor.SeparateToSingleWords(content, delimiter);
+        var results = _textProcessor.CountWordsOccurrences(words);
         
-        ResultWindow.Text = content;
+        foreach (var wordWithOccurrence in results)
+        {
+            var item = new TextFileResult{ Word = wordWithOccurrence.Item1, Occurrence = wordWithOccurrence.Item2 };
+            ResultList.Items.Add(item);
+        }
+
+        ResultList.Visibility = Visibility.Visible;
+    }
+
+    private void Cancel_Click(object sender, RoutedEventArgs e)
+    {
+        return;
+    }
+
+    private void CollapseResults()
+    {
+        ProgressBar.Visibility = Visibility.Collapsed;
+        ProgressBarLabel.Visibility = Visibility.Collapsed;
+        ResultMessage.Visibility = Visibility.Collapsed;
+        ResultList.Visibility = Visibility.Collapsed;
+        Cancel.Visibility = Visibility.Collapsed;
     }
 }
