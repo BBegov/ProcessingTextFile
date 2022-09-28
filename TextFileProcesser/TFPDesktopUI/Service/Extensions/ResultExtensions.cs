7eadc315ce34f1ad4722cd6f934a94093dbab0e1
Balázs Begov <bbegov@googlemail.com>
2022-09-28 14:35:19 +0200

    Add: extension created to convert result to TextFileResult class

---
+++ TextFileProcesser/TFPDesktopUI/Service/Extensions/ResultExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TFPDesktopUI.Models;
+
+namespace TFPDesktopUI.Service.Extensions;
+
+public static class Extensions
+{
+    public static TextFileResult ToTextFileResults(this (string, int) wordsWithOccurrence)
+    {
+        return new TextFileResult
+        {
+            Word = wordsWithOccurrence.Item1,
+            Occurrence = wordsWithOccurrence.Item2
+        };
+    }
+
+    public static List<TextFileResult> ToTextFileResults(this (string, int)[] wordsWithOccurrences)
+    {
+        return wordsWithOccurrences.Select(ToTextFileResults).ToList();
+    }
+}
