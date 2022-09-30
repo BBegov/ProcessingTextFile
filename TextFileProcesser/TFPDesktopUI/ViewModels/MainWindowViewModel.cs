8b3d60c5c210f071c3b422998f3258401d43d64d
Balázs Begov <bbegov@googlemail.com>
2022-09-30 14:05:42 +0200

    Add: progress and cancellation

--- TextFileProcesser/TFPDesktopUI/ViewModels/MainWindowViewModel.cs
+++ TextFileProcesser/TFPDesktopUI/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
@@ -13,21 +15,25 @@ public partial class MainWindowViewModel : ObservableObject
 {
     private readonly IFileHandler _fileHandler;
     private readonly ITextProcessor _textProcessor;
+
+    private CancellationTokenSource? _tokenSource;
     
     [ObservableProperty]
     private List<TextFileResult> _textFileResults = new();
 
     [ObservableProperty]
-    [NotifyCanExecuteChangedFor(nameof(BrowseFileCommand))]
     private string _filePath = string.Empty;
 
     [ObservableProperty]
-    [NotifyCanExecuteChangedFor(nameof(AnalyzeCommand))]
     private string _infoMessage = string.Empty;
 
     [ObservableProperty]
     [NotifyCanExecuteChangedFor(nameof(AnalyzeCommand))]
-    private int _percentageComplete;
+    private int _progressbarValue;
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(AnalyzeCommand))]
+    private string? _percentageComplete;
 
     public MainWindowViewModel(IFileHandler fileHandler, ITextProcessor textProcessor)
     {
@@ -35,18 +41,24 @@ public partial class MainWindowViewModel : ObservableObject
         _textProcessor = textProcessor;
     }
 
-    private bool CanAnalyze() 
-        => FilePath != string.Empty;
-
+    private bool CanCancel()
+        => ProgressbarValue != 0 && ProgressbarValue != 100;
     
-    [RelayCommand]
+    [RelayCommand(CanExecute = nameof(CanCancel))]
     private void Cancel()
     {
+        _tokenSource?.Cancel();
     }
 
     [RelayCommand]
     private async Task Analyze()
     {
+        var progress = new Progress<int>();
+        progress.ProgressChanged += ReportProgress;
+
+        _tokenSource = new CancellationTokenSource();
+        var token = _tokenSource.Token;
+
         if (FilePath == string.Empty)
         {
             InfoMessage = "Browse a file first!";
@@ -63,22 +75,27 @@ public partial class MainWindowViewModel : ObservableObject
                 return;
             }
 
-            //Reading the file
-            InfoMessage = "1. Reading file...";
-            var fileContent = await _fileHandler.ReadFileByLines(FilePath);
-            PercentageComplete = 50;
-            InfoMessage += "Done";
-
-            //Processing the file
-            InfoMessage += "\n2. Processing file...";
-            await Task.Run(() => ProcessFileContent(fileContent));
-            PercentageComplete = 100;
-            InfoMessage += "Done";
+            await FileParsing(progress, token);
+        }
+        catch (OperationCanceledException)
+        {
+            InfoMessage = "File parsing was canceled.";
         }
         catch (IOException exception)
         {
-            InfoMessage = $"An exception occurred:\nError code: {exception.HResult & 0x0000FFFF}\nMessage: {exception.Message}";
+            InfoMessage =
+                $"An exception occurred:\nError code: {exception.HResult & 0x0000FFFF}\nMessage: {exception.Message}";
         }
+        finally
+        {
+            _tokenSource.Dispose();
+        }
+    }
+
+    private void ReportProgress(object? sender, int e)
+    {
+        ProgressbarValue = e;
+        PercentageComplete = $"{e}%";
     }
 
     [RelayCommand]
@@ -101,7 +118,22 @@ public partial class MainWindowViewModel : ObservableObject
         FilePath = dialog.FileName;
     }
 
-    private void ProcessFileContent(string[] fileContent)
+    private async Task FileParsing(IProgress<int> progress, CancellationToken token)
+    {
+        //Reading the file
+        InfoMessage = "1. Reading file...";
+        var fileContent = await _fileHandler.ReadFileByLinesAsync(FilePath, progress, token);
+        InfoMessage += "Done";
+
+        token.ThrowIfCancellationRequested();
+
+        //Processing the file
+        InfoMessage += "\n2. Processing file...";
+        await Task.Run(() => ProcessFileContent(fileContent), token);
+        InfoMessage += "Done";
+    }
+
+    private void ProcessFileContent(string fileContent)
     {
         var singleWords = _textProcessor.SeparateTextToSingleWords(fileContent);
         var wordsWithOccurrences = _textProcessor.CountWordsOccurrences(singleWords);
