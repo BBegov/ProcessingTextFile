708361f68a34fbd209a7dd9676bb0c0ebf57b473
Balázs Begov <bbegov@googlemail.com>
2022-10-01 18:58:39 +0200

    Refactoring: counting words and ordering separated

--- TextFileProcesser/TFPLibrary/TextProcessor.cs
+++ TextFileProcesser/TFPLibrary/TextProcessor.cs
@@ -6,32 +6,32 @@ public static class TextProcessor
 {
     public static string[] SeparateTextToSingleWords(string text, string delimiter = " ")
     {
-        return text
-            .CleanText()
-            .Split(delimiter)
-            .ToArray();
+        return text.CleanText().Split(delimiter);
     }
 
     private static string CleanText(this string text)
     {
-        return text.Length == 0 
-            ? string.Empty 
-            : Regex
-                .Replace(text, @"\s+", " ")
-                .Trim();
+        return text.Length == 0
+            ? string.Empty
+            : Regex.Replace(text, @"\s+", " ").Trim();
     }
 
-    public static (string, int)[] CountWordsOccurrences(string[] words)
+    public static Dictionary<string, int> CountWordsOccurrences(string[] words)
     {
         var wordsAndCounts = new Dictionary<string, int>();
 
         foreach (var word in words)
         {
-            wordsAndCounts[word] = wordsAndCounts.ContainsKey(word) 
-                ? wordsAndCounts[word] + 1 
+            wordsAndCounts[word] = wordsAndCounts.ContainsKey(word)
+                ? wordsAndCounts[word] + 1
                 : 1;
         }
 
+        return wordsAndCounts;
+    }
+
+    public static (string, int)[] ConvertToDescendingArray(IDictionary<string, int> wordsAndCounts)
+    {
         return wordsAndCounts
             .Select(kvp => (kvp.Key, kvp.Value))
             .OrderByDescending(kvp => kvp.Value)
