3d1c612e4929543389b39d53d75bd1217ded95a6
Balázs Begov <bbegov@googlemail.com>
2022-10-01 09:06:30 +0200

    Change: Interfaces deleted

--- TextFileProcesser/TFPLibrary/ITextProcessor.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TFPLibrary;
-
-public interface ITextProcessor
-{
-    string[] SeparateTextToSingleWords(string text, string delimiter = " ");
-    (string, int)[] CountWordsOccurrences(string[] words);
-}
