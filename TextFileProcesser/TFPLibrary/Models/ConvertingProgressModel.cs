f9957eaf84ecd5f9a61bb0db77c21d7d77c46141
Balázs Begov <bbegov@googlemail.com>
2022-10-03 23:06:19 +0200

    Add: progress report models to parsing steps

---
+++ TextFileProcesser/TFPLibrary/Models/ConvertingProgressModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TFPLibrary.Models;
+
+public class ConvertingProgressModel : IReportProgress
+{
+    public Dictionary<string, int> WordsAndCounts { get; set; } = new();
+    public List<(string, int)> UnorderedResultList { get; set; } = new();
+
+    public int ReportProgress() => WordsAndCounts.Count == 0
+        ? 0
+        : UnorderedResultList.Count * 100 / WordsAndCounts.Count;
+}
---
+++ TextFileProcesser/TFPLibrary/Models/SplittingProgressModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TFPLibrary.Models;
+
+public class SplittingProgressModel : IReportProgress
+{
+    public List<string> Lines { get; set; } = new();
+    public HashSet<string> ProgressedLines { get; set; } = new();
+
+    public int ReportProgress() => Lines.Count == 0
+        ? 0
+        : ProgressedLines.Count * 100 / Lines.Count;
+}
