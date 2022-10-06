2b1391004f97dd1136cf683fe5f2ab533c83378b
Balázs Begov <bbegov@googlemail.com>
2022-10-06 10:03:28 +0200

    Add: static extension method to strings

---
+++ TextFileProcesser/TFPLibrary/Extensions/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TFPLibrary.Extensions;
+
+internal static class TextProcessor
+{
+    internal static string ReduceWhiteSpacesToSingle(this string text)
+    {
+        return string.IsNullOrEmpty(text)
+            ? string.Empty
+            : Regex.Replace(text, @"\s+", " ").Trim();
+    }
+}
