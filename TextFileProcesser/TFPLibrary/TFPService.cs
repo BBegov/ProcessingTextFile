5b800c502f068c72ede34f6bba35d56ae81b4fe9
Balázs Begov <bbegov@googlemail.com>
2022-10-06 10:06:36 +0200

    Change: service class renamed and simplyfied

---
+++ TextFileProcesser/TFPLibrary/TFPService.cs
@@ -0,0 +1,12 @@
+﻿using TFPLibrary.Models;
+
+namespace TFPLibrary;
+public static class TFPService
+{
+    public static async Task<(string, int)[]> ParseFileContentAsync(string[] textLines, IProgress<int> progress, CancellationToken ct)
+    {
+        var reportingModel = new ProgressReportingModel();
+        var fileContentParser = new FileParser(reportingModel, progress, ct);
+        return await fileContentParser.ParseFileContentAsync(textLines);
+    }
+}
